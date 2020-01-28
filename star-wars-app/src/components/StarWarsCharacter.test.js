e7127f01f784bdafc52e96bd24bda365f194688b
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-01-28 07:40:08 -0600

    A little more cleanup

--- star-wars-app/src/components/StarWarsCharacter.test.js
+++ star-wars-app/src/components/StarWarsCharacter.test.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom'
 import '@testing-library/jest-dom/extend-expect';
 import {render, fireEvent, wait} from '@testing-library/react';
 import {getByTestId} from '@testing-library/dom'
-import {getData} from '../api/index';
+import {getData as mockGetData} from '../api/index';
 import StarWarsCharacters from './StarWarsCharacters'
 
 jest.mock('../api')
 
 
 
+
+
 test('Buttons Next and Previous function as intended', async() =>{
 
-    getData.mockResolvedValue(
+   const testData =
         {
           results: [
             {
@@ -23,21 +25,22 @@ test('Buttons Next and Previous function as intended', async() =>{
           next: 'testNext',
           previous: 'testPrevious'
         }
-    )
 
-const { getByText } = render(<StarWarsCharacters />)
+    mockGetData.mockResolvedValueOnce(testData)
 
-const testNextButton = getByText(/next/i)
-const testPreviousButton = getByText(/previous/i)
+    const { getByText } = render(<StarWarsCharacters />)
+
+    const testNextButton = getByText(/next/i)
+    const testPreviousButton = getByText(/previous/i)
 
 
 
-fireEvent.click(testNextButton)
-fireEvent.click(testPreviousButton)
-expect(getData).toHaveBeenCalledTimes(1)
+    fireEvent.click(testNextButton)
+    fireEvent.click(testPreviousButton)q
+    expect(mockGetData).toHaveBeenCalledTimes(1)
 
-wait(() => expect(getByText(/'Darth'/i)))
-})
+    wait(() => expect(getByText(/'Darth'/i)))
+    })
 
 
 
